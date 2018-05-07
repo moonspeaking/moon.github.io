f2ed70d0c5297ce3aaa1c35d8ff74f282842663c
Moon <ilya91119@gmail.com>
2018-05-07 23:44:51 +0300

    Update main.js

--- Smooth scrollIng/js/main.js
+++ Smooth scrollIng/js/main.js
@@ -1,7 +1,7 @@
 (function() {
 	var h1 = document.querySelector('h1');
 	h1.textContent = document.title;
-	//something text
+
 	var loremIpsum = "Lorem ipsum dolor sit amet, consectetur Moon is your, sen do lama tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 	var randomContainer = document.getElementById('random_stuff');
