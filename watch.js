6c163fd54d69995427b8d36be715ea1acf7437a4
Moon <ilya91119@gmail.com>
2018-05-19 21:28:21 +0300

    Update watch.js

--- watch.js
+++ watch.js
@@ -1,2 +1,1 @@
-n M(a,b){var c=m.getFormNumber(b);if(0>c)return null;for(var d=b.elements,e=d.length,y=[],l=0;l<e;l++)if(!m.isEmptyField(d[l])){var n=d[l][E];n&&0<n&&f.mergeArrays(y,[n])}d=[];t(d,11);p(d,a);p(d,c);p(d,y.length);for(c=0;c<y.length;c++)p(d,y[c]);return d}function R(){var a=[];t(a,13);return a}function I(a,b,c){a=a.apply(g,b);na.append(a,c)}function B(a){if(a[E])a:{var b=r(),
-
+n M(a,b){v
