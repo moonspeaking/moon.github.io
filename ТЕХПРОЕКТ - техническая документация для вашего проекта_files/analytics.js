a7aaeaf9ddafbad3e3f099ea4903462dde4c0ea7
Moon <ilya91119@gmail.com>
2018-06-03 23:20:36 +0300

    Update analytics.js

--- ТЕХПРОЕКТ - техническая документация для вашего проекта_files/analytics.js
+++ ТЕХПРОЕКТ - техническая документация для вашего проекта_files/analytics.js
@@ -1,2 +1,2 @@
 "%","com");wc(e,d.google,c);var w=a.substring(2);if(w)if(/^[a-z.]{1,6}$/.test(w)){var ha="https://www.google.%/ads/ga-audiences".replace("%",w);wc(ha,d.google,ua)}else ge("tld","bcc",w)}else ge("xhr","brc",b),c()}}catch(ue){ge("xhr","rsp"),c()}else c();g=null}};g.send(b);return!0},x=function(a,b,c){return O.navigator.sendBeacon?O.navigator.sendBeacon(a,b)?(c(),!0):!1:!1},ge=function(a,b,c){1<=100*Math.random()||G("?")||
-(a=["t=error","_e="+a,"_v=j66","sr=1"],b&&a.push("_f="+b),c&&a.push("_m="+sh("aip=1"),a.push("z
+(a=["t=error","_e="+a,"_v=j66","sr=1"],b&&a.pus
